d463957e1da4a54084df7c84b530f1daa3a5c4c0
BrunG9 <brunoograssi@hotmail.com>
2022-07-04 15:16:49 -0300

    Modulo Javascript, mjs e js

---
+++ javascript/Modulo/main.js
@@ -0,0 +1,5 @@
+import {mostrarIdade, mostraCidade, mostraHobby} from './funcoes.mjs';
+
+console.log(mostrarIdade('Bruno', 21));
+console.log(mostraCidade('Bruno', 'SP'));
+console.log(mostraHobby('Bruno', 'Jogar'));
